a6d8f28ff893cd3b903cbf47634347ed0127c87e
Cauã Alves Lacerda <caua.al@outlook.com>
2025-07-12 01:09:01 -0300

    Create arrayTipos.js

---
+++ arrayTipos.js
@@ -0,0 +1,14 @@
+// Array Strings    
+const alunos = ['João', 'Diego', 'Eduardo', 'Amanda'];
+
+const produtos = ["Notebook x40", "iPhone X", "Mouse Microsoft"];
+
+//Array de number
+const valores = [ 4010.99, 10000.76, 90.15 ];
+
+//Array de objetos
+const itensVenda = [
+    { produto: "Notebook x40", valor: 4010.99, ativo: true },
+    { produto: "iPhone X", valor: 10000.76, ativo: true },
+    { produto: "Mouse Microsoft", valor: 90.15, ativo: false },
+];
