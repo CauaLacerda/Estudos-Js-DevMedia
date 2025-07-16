7f8d8d33b27bce8cfd985308ecbfd8c0e80d520a
Cauã Alves Lacerda <caua.al@outlook.com>
2025-07-15 23:23:44 -0300

    Update foreach.js

--- foreach.js
+++ foreach.js
@@ -1,3 +1,24 @@
+const produtos = [
+  { id: 1, nome: 'Açucar', estoque: 100, valor: 2.00 },
+  { id: 2, nome: 'Álcool 70', estoque: 50, valor: 9.95 },
+  { id: 3, nome: 'Luvas descartáveis', estoque: 1000, valor: 2.50 },
+];
+
+function imprimirProduto (produto, index) {
+  console.log( index + ' - ' + produto.nome );
+}
+
+produtos.forEach(imprimirProduto);
+
+
+
+
+
+
+
+
+
+
 const carrinho = [
   {  nome: 'The Legend of Zelda', qnt: 1, valor: 250 },
   {  nome: 'Super Mario Kart 8', qnt: 1, valor: 300 },
