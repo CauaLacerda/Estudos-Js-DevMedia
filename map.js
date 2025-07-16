e08417770919e493a7960559c31dd97ad3bfd06b
Cauã Alves Lacerda <caua.al@outlook.com>
2025-07-16 01:35:28 -0300

    Update map.js

--- map.js
+++ map.js
@@ -13,3 +13,16 @@ const novosCarros = carros.map(retornaCarro);
 
 console.log(novosCarros);
 
+const meses = [
+    "Janeiro", "Fevereiro", "Março", "Abril",
+    "Maio", "Junho", "Julho", "Agosto",
+    "Setembro", "Outubro", "Novembro", "Dezembro"
+  ];
+
+  function abreviar(mes) {
+    return mes.substr(0,3)
+  }
+
+  const mesesAbreviados = meses.map(abreviar);
+
+  console.log(mesesAbreviados);
