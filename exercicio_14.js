9781fe6abf998b2b761d5aa50833c6a05abcd80c
Cauã Alves Lacerda <caua.al@outlook.com>
2025-07-24 01:57:08 -0300

    Create exercicio_14.js

---
+++ exercicio_14.js
@@ -0,0 +1,12 @@
+function celsius_fahrenheit (temperatura_celsius)
+{
+
+    let temperatura_fahrenheit = temperatura_celsius * 1.8 + 32;
+    
+return temperatura_fahrenheit;
+
+
+}
+
+let resultado = celsius_fahrenheit(20);
+console.log(resultado);
